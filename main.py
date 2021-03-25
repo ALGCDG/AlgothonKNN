d23f93aa8aacac587bdd3c7a8876c418c7da2f75
Archie <ac11018@ic.ac.uk>
2021-03-25 23:20:15 +0000

    changing training data and making non mac specific

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-#!/usr/local/bin/python3
 from sklearn.neighbors import KNeighborsClassifier
-
-def classify(x, k, training_x, training_y):
-    def distance(a, b):
-        return sum([(c-d)**2 for c,d in zip(a, b)])
-    nearest = list(zip(training_x, training_y)).sort(key=lambda y: distance(y[0],x))[:k]
-    ys=[b for a,b in nearest]
-    if sum(ys)>(len(ys)/2):
-        return 1
-    else:
-        return 0
-
-
 from sys import stdin
 import pandas as pd
-WINDOWSIZE=5
-data = list(pd.read_csv('LatencyChallengeTraining.csv')['Log Returns'])
+
+WINDOWSIZE=5 # windowsize input 
+K=10 # K parameter for KNN algorithm  
+
+# load training data
+data = list(pd.read_csv('training.csv')['LogReturns'])
+
+# convert time series into training examples
 x_train = []
 y_train = []
 for i in range(len(data)-WINDOWSIZE):
     x_train.append(data[i:i+WINDOWSIZE])
     y_train.append(round(data[i+WINDOWSIZE]))
-#print(x_train)
-#print(None in x_train)
-#for i in zip(x_train,y_train):
-#    print(i)
-#for i in map(lambda x: x[0], zip(x_train,y_train)):
-#    print(i)
-classifier=KNeighborsClassifier(n_neighbors=10)
+
+# declare and train classifier
+classifier=KNeighborsClassifier(n_neighbors=K)
 classifier.fit(x_train, y_train)
+
+# classify terminal input
 for line in stdin:
     if line == '': 
         break
